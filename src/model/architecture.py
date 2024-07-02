Fix broadcasting issue in apply_rotary_pos_emb

src/model/architecture.py
@@ -25,11 +25,11 @@ def apply_rotary_pos_emb(x, sincos):
     sin, cos = sincos
     x1, x2 = jnp.split(x, 2, axis=-1)
     x_rotated = jnp.concatenate([-x2, x1], axis=-1)
-    sin = sin.reshape(1, 32, 2, 32)
-    cos = cos.reshape(1, 32, 2, 32)
+    sin = sin.reshape(1, 1, 32, 16)
+    cos = cos.reshape(1, 1, 32, 16)
     sin1, sin2 = jnp.split(sin, 2, axis=-1)
     cos1, cos2 = jnp.split(cos, 2, axis=-1)
-    result = (x1 * cos1) + (x_rotated * sin1)
+    result = (x1 * cos) + (x_rotated * sin)
     return jnp.concatenate([result, x2], axis=-1)
 
 